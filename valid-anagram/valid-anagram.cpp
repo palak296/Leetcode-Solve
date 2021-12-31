0041c88cfccabd1fcd412ce7953b31313d4e05f5
Palak Garg <palakgarg296@gmail.com>
2021-12-31 17:27:06 +0530

    Time: 19 ms (8.87%), Space: 7.2 MB (76.70%) - LeetHub

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -1,19 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-      if(s.size()!=t.size()) return false;
-        unordered_map<char,int>mp;
-
-        for(int i=0;i<s.size();i++)
-        {
-          mp[s[i]]++;
-          mp[t[i]]--;
-        }
-      
-      for(auto it: mp)
-      {
-        if(it.second) return false;
-      }
-      return true;
+        sort(s.begin(),s.end());
+      sort(t.begin(),t.end());
+      if(s==t) return true;
+      return false;
     }
 };
