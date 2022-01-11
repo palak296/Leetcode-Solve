180d5fa28405b1c911f5a04afec64d28544c0d0d
Palak Garg <palakgarg296@gmail.com>
2022-01-11 10:51:22 +0530

    Added solution - LeetHub

--- Max Sum Subarray of size K - GFG/max-sum-subarray-of-size-k.cpp
+++ Max Sum Subarray of size K - GFG/max-sum-subarray-of-size-k.cpp
@@ -7,17 +7,15 @@ class Solution{
 public:
     int maximumSumSubarray(int k, vector<int> &arr , int N){
         // code here 
-        int i=0,j=0,mx=0,sum=0;
+        int i=0,j=0,sum=0,mx=INT_MIN;
         while(j<N)
         {
-            sum+=arr[j++];
+            sum=sum+arr[j++];
             if(j-i==k)
             {
-                
                 mx=max(mx,sum);
                 sum=sum-arr[i++];
             }
-            
         }
         return mx;
     }
