ed38940238979bfffd39c4a600cfa84a127959b3
Palak Garg <palakgarg296@gmail.com>
2022-01-16 17:07:58 +0530

    Time: 4 ms (100.00%), Space: 5.8 MB (100.00%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    int f(int i, int d){
-        if(i == 1) return 0; // reached 
-        if(d == 0) return i - 1; // only way left to reach 1 in i - 1 operations
-        
-        int ans = 0;
-        
-        if(i & 1){ // when i is odd change it to even
-            ans = 1 + f(i - 1, d);
-        }
-        else if(d > 0){ // when i is even and we have maxDoubles left then use it
-            ans = 1 + f(i / 2, d - 1);
-        }
-        
-        return ans;
-    }
     int minMoves(int target, int maxDoubles) {
-        return f(target, maxDoubles); // move reverse from target to 1 using / and - operator.
+        if(maxDoubles==0)
+            return target-1;
+        long long d=0;
+        while(target>1 && --maxDoubles>=0){
+            d++;
+            if(target%2!=0)
+                d++;
+            target=target/2;
+        }
+        return d+target-1;
     }
 };
