8f516b3e72bebf5cd18177c7f2daf3068f8c62ea
Palak Garg <palakgarg296@gmail.com>
2022-01-21 11:21:52 +0530

    Time: 15 ms (12.25%), Space: 13.7 MB (58.84%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+  void find(string ds, vector<string>&ans, int o, int c)
+  {
+    //int o=2,c=3;
+    if(o==0 and c==0)
+    {
+      
+  
+      ans.push_back(ds);
+      return;
+    }
+    if(o>0)
+    {
+      ds+='(';
+      find(ds,ans,o-1,c);
+      ds.pop_back();
+        
+      
+    }
+    
+    
+  if(o<c)
+    
+    find(ds+')',ans,o,c-1);
+    
+    
+  }
+    vector<string> generateParenthesis(int n) {
+      
+      string ds;
+      vector<string> ans;
+      int o=n,c=n;
+      
+      find(ds,ans,o,c);
+      return ans;
+    }
+};
