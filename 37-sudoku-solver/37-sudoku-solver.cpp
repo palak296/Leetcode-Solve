7fb26da55605c889e4b05b9d87c0042ffc14c315
Palak Garg <palakgarg296@gmail.com>
2022-01-22 12:37:26 +0530

    Time: 54 ms (26.48%), Space: 6.3 MB (96.26%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+  bool solve(vector<vector<char>>& b)
+  {
+    for(int i=0;i<b.size();i++)
+    {
+      for(int j=0;j<b[0].size();j++)
+      {
+        if(b[i][j]=='.')
+        {
+          for(char k='1';k<='9';k++)
+          {
+           if(valid(i,j,k,b))
+            {
+              b[i][j]=k;
+            if(solve(b)==true) return true;
+            else b[i][j]='.';
+           }
+          }
+          return false;
+        }
+      }
+    }
+    return true;
+  } 
+  bool valid(int row, int col, char c,vector<vector<char>>& b )
+  {
+    for(int i=0;i<9;i++)
+    {
+      if(b[i][col]==c) return false;
+      if(b[row][i]==c) return false;
+      if(b[3*(row/3)+i/3][3*(col/3)+i%3]==c) return false;
+      
+    }
+    return true;
+  }
+};
