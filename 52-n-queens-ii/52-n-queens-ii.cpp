a676459c5b4caca3e41cac191e87d3aa8aa0f235
Palak Garg <palakgarg296@gmail.com>
2022-01-23 13:14:27 +0530

    Time: 129 ms (6.15%), Space: 6.6 MB (32.77%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,63 +1,59 @@
 class Solution {
 public:
-   bool safe(int row, int col, int n, vector<string>&b)
+  bool safe(int row, int col, vector<vector<string>>& ans, vector<string>&ds, int n)
   {
-    int dr=row;
-    int dc=col;
-    
+    cout<<row<<" "<<col;
+    int rr=row;
+    int cc=col;
     while(row>=0 and col>=0)
     {
-      if(b[row][col]=='Q') return false;
+      if(ds[row][col]=='Q') return false;
       row--; col--;
     }
-    row=dr;
-    col=dc;
+    row=rr;
+    col=cc;
+    
     while(col>=0)
     {
-      if(b[row][col]=='Q') return false;
+      if(ds[row][col]=='Q') return false;
       col--;
     }
-    row=dr;
-    col=dc;
+    row=rr;
+    col=cc;
+    
     while(row<n and col>=0)
     {
-      if(b[row][col]=='Q') return false;
-      row++;
-      col--;
+      if(ds[row][col]=='Q') return false;
+      row++; col--;
     }
     return true;
   }
-    
- void find(int col,int n, vector<string>& board, vector<vector<string>>& ans)
+  void find(int col, int n, vector<vector<string>>& ans, vector<string>&ds)
   {
     if(col==n)
     {
-      ans.push_back(board);
-     return;
+      ans.push_back(ds);
+      return;
     }
     for(int i=0;i<n;i++)
     {
-      if(safe(i,col,n,board))
+      if(safe(i,col,ans,ds,n))
       {
-        board[i][col]='Q';
-        find(col+1,n,board,ans);
-        board[i][col]='.';
+        ds[i][col]='Q';
+        find(col+1,n,ans,ds);
+        ds[i][col]='.';
       }
     }
-  } 
- 
+  }
     int totalNQueens(int n) {
-      vector<vector<string>> ans;
-    string s(n,'.');
-      vector<string> board(n);
+     vector<vector<string>>ans;
+      vector<string>ds(n);
+      string s(n,'.');
       for(int i=0;i<n;i++)
       {
-        board[i]=s;
+        ds[i]=s;
       }
-      find(0,n,board,ans);
-    return ans.size();
+      find(0,n,ans,ds);
+      return ans.size();
     }
 };
-
-
- 
