17bdd24a83b5ba04fbac8ce910cde8b6644ddd84
Palak Garg <palakgarg296@gmail.com>
2022-02-07 13:19:52 +0530

    Time: 34 ms (54.15%), Space: 18.9 MB (49.79%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& in) {
+      //  if(in.size()<=1) return in;
+      sort(in.begin(),in.end());
+      vector<vector<int>>v;
+      v.push_back(in[0]);
+      for(int i=1;i<in.size();i++)
+      {
+        if(v.back()[1]>=in[i][0]) v.back()[1]=max(v.back()[1],in[i][1]);
+        else
+          v.push_back(in[i]);
+      }
+      return v;
+    }
+};
