291f93c136d7d7622060eb66f8d67d0da0f93a66
Palak Garg <palakgarg296@gmail.com>
2022-02-13 10:21:45 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (85.44%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,24 +1,21 @@
+
 class Solution {
 public:
-  void find(int i, set<vector<int>>&s, vector<int>&ds, vector<int>&nums){
-    int n=nums.size();
-    if(i==n) {
-      s.insert(ds);;
-      return;
-    }
-    ds.push_back(nums[i]);
-    find(i+1,s,ds,nums);
-    ds.pop_back();
-    find(i+1,s,ds,nums);
+  void find(int n, vector<int>& nums, vector<int>&v, vector<vector<int>>&ans)
+  {
+    if(n==nums.size())
+    { ans.push_back(v);
+    return ;}
+      v.push_back(nums[n]);
+      find(n+1,nums,v,ans);
+      v.pop_back();
+      find(n+1,nums,v,ans);
+    
   }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>v;
-      set<vector<int>>s;
-      vector<int>ds;
-      find(0,s,ds,nums);
-      for(auto x: s){
-        v.push_back(x);
-      }
-      return v;
+        vector<vector<int>>ans;
+      vector<int>v;
+      find(0,nums,v,ans);
+        return ans;
     }
 };
