8cdac4839a7ddd401d8368d4f873fedd9296cc5c
Palak Garg <palakgarg296@gmail.com>
2022-02-14 11:59:07 +0530

    Time: 25 ms (25.04%), Space: 21 MB (52.40%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -11,21 +11,23 @@
  */
 class Solution {
 public:
+    bool isBalanced(TreeNode* root) {
+        if(check(root)==-1)
+          return false;
+      return true;
+      
+    }
   int check(TreeNode* root)
   {
-    
-    if(root==NULL) return 0;
+    if(root==NULL)
+    {
+      return 0;
+    }
     int lh=check(root->left);
     if(lh==-1) return -1;
     int rh=check(root->right);
     if(rh==-1) return -1;
     if(abs(lh-rh)>1) return -1;
-    return max(lh,rh)+1;
+    return 1+max(lh,rh);
   }
-    bool isBalanced(TreeNode* root) {
-        
-      if(check(root)==-1)
-        return false;
-      return true;
-    }
 };
