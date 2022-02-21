0f48403a9302a3aef2ede212df342a9149d13175
Palak Garg <palakgarg296@gmail.com>
2022-02-21 19:01:48 +0530

    Time: 49 ms (52.18%), Space: 71.5 MB (49.53%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -10,28 +10,17 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-     
-     ListNode *nxt = NULL, *prev = NULL;
-    int i = 0;
-    while (l1 || l2) {
-        int v1 = l1? l1->val: 0;
-        int v2 = l2? l2->val: 0;
-        int tmp = v1 + v2 + i;
-        i = tmp / 10;
-        int val = tmp % 10;
-        ListNode* cur = new ListNode(val);
-        if (!nxt) nxt = cur;
-        if (prev) prev->next = cur;
-        prev = cur;
-        l1 = l1? l1->next: NULL;
-        l2 = l2? l2->next: NULL;
+   ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+    ListNode preHead(0), *p = &preHead;
+    int extra = 0;
+    while (l1 || l2 || extra) {
+        int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + extra;
+        extra = sum / 10;
+        p->next = new ListNode(sum % 10);
+        p = p->next;
+        l1 = l1 ? l1->next : l1;
+        l2 = l2 ? l2->next : l2;
     }
-    if (i > 0) {
-        ListNode* l = new ListNode(i);
-        prev->next = l;
-    }
-      return nxt;
-        
-    }
+    return preHead.next;
+}
 };
