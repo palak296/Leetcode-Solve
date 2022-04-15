e18123c08eadcba3e722124e048a321356dc310a
Palak Garg <palakgarg296@gmail.com>
2022-04-15 12:33:51 +0530

    Time: 19 ms (82.26%), Space: 19.4 MB (99.89%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-      int s=nums.size();
-      return nums[s/2];
+      return nums[nums.size()/2];
     }
 };
