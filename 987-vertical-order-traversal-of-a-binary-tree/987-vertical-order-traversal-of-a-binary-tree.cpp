667375f800dfd96bac4fb3b2a15bc01899b62f52
Palak Garg <palakgarg296@gmail.com>
2022-04-16 10:52:22 +0530

    Time: 7 ms (60.64%), Space: 13.4 MB (51.19%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,5 +1,5 @@
 /**
- * Definition for a binary tree node.
+ * Definition for a binary tree node.m
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
@@ -12,32 +12,31 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int,map<int,multiset<int>>> nodes;
-      queue<pair<TreeNode*, pair<int,int>>> todo;
-      todo.push({root,{0,0}});
-      while(!todo.empty())
-      {
-        auto p=todo.front();
-        TreeNode* nn=p.first;
-        todo.pop();
-        int x=p.second.first, y=p.second.second;
-        nodes[x][y].insert(nn->val);
-        if(nn->left)
-          todo.push({nn->left,{x-1,y+1}});
-        if(nn->right)
-          todo.push({nn->right,{x+1,y+1}});
-      }
-      vector<vector<int>>v;
-      for(auto it: nodes)
-      {
-        vector<int>col;
-        for(auto p: it.second)
-        {
-          col.insert(col.end(),p.second.begin(),p.second.end());
-          
+        map<int,map<int,multiset<int>>> m;
+        queue<pair<TreeNode*,pair<int,int>>> q;
+        q.push({root,{0,0}});
+        while(!q.empty()){
+            auto p=q.front();
+           TreeNode* node=p.first;
+            q.pop();
+            int x=p.second.first, y=p.second.second;
+            m[x][y].insert(node->val);
+            
+                if(node->left) q.push({node->left,{x-1,y+1}});
+                if(node->right) q.push({node->right,{x+1,y+1}});
+                
+            
         }
-        v.push_back(col);
-      }      
-      return v;
+       vector<vector<int>>ans;
+        for(auto i: m)
+        {
+            vector<int>v;
+            for(auto j: i.second)
+            {
+                v.insert(v.end(),j.second.begin(),j.second.end());
+            }
+            ans.push_back(v);
+        }
+        return ans;
     }
 };
