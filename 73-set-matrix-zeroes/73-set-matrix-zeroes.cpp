6f10e0973c36996697c4d97af0db09229afe8923
Palak Garg <palakgarg296@gmail.com>
2022-06-30 11:24:57 +0530

    Time: 27 ms (29.54%), Space: 13.3 MB (52.95%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-     void setZeroes(vector<vector<int>>& a) {
-        int n = a.size();
-        int m = a[0].size();
-        bool firstRow = false;  
-        bool firstCol = false; 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-					if(a[i][j] == 0){ 
-                    if(i==0) firstRow = true;
-                    if(j==0) firstCol = true;
-                    a[i][0] = 0;
-                    a[0][j] = 0;
+    void setZeroes(vector<vector<int>>& m) {
+        int r=m.size();
+        int c=m[0].size();
+        vector<int>row(r,-1);
+        vector<int>col(c,-1);
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                if(m[i][j]==0)
+                {   row[i]=0; col[j]=0;}
+            }
+        }
+        for(int i=0;i<r;i++) cout<<row[i]<<" ";
+            cout<<endl;
+          for(int i=0;i<c;i++) cout<<col[i]<<" ";
+        for(int i=0;i<r;i++)
+        {
+            if(row[i]==0){
+                for(int j=0;j<c;j++){
+                    m[i][j]=0;
                 }
             }
         }
-       
-        for(int i=1; i<n; i++){
-            for(int j=1; j<m; j++){
-                if(a[i][0] == 0 || a[0][j] == 0){
-                    a[i][j] = 0;
+        for(int i=0;i<c;i++)
+        {
+            if(col[i]==0){
+                for(int j=0;j<r;j++){
+                    m[j][i]=0;
                 }
             }
-        }
-        
-        if(firstRow){
-            for(int i=0;i<m;i++) a[0][i] = 0;
-        }
-        
-        if(firstCol){
-            for(int i=0;i<n;i++) a[i][0] = 0;
         }
     }
 };
