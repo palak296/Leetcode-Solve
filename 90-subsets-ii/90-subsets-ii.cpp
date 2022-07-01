b27f90c973a480094337a9bc41f07a0b3cdf4159
Palak Garg <palakgarg296@gmail.com>
2022-07-01 12:29:31 +0530

    Time: 4 ms (68.85%), Space: 7.9 MB (46.58%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-  void find( int i, set<vector<int>>&a, vector<int>&ds, vector<int>& nums){
-    int n=nums.size();
-    if(i==n){
-      a.insert(ds);     return;
-    }
-    ds.push_back(nums[i]);
-    find(i+1,a,ds,nums);
-    ds.pop_back();
-    int ind=i+1;
-      while(ind<n and nums[ind]==nums[i] )
-      {
-        ind++;
-      }
-    find(ind,a,ds,nums);
-  }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>>a;
-      vector<vector<int>>v;
-      vector<int>ds;
-      sort(nums.begin(),nums.end());
-      find(0,a,ds,nums);
-      for(auto x: a)
-      {
-        v.push_back(x);
-      }
-      return v;
-      
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+       vector<vector<int>>ans;
+        vector<int>v;
+        set<vector<int>>st;
+        
+        cal(nums,v,st,0,n);
+       for(auto x: st)
+           ans.push_back(x);
+       
+        return ans;
+    }
+    void cal(vector<int>&nums, vector<int>&v, set<vector<int>>&st,int i,int n)
+    {
+        if(i==n)
+        {
+            st.insert(v);
+            return;
+        }
+        //cout<<nums[i]<<" ";
+        v.push_back(nums[i]);
+        cal(nums,v,st,i+1,n);
+        v.pop_back();
+        cal(nums,v,st,i+1,n);
     }
 };
