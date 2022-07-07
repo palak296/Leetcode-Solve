dbcd8ad8b2c6d0935834c0eb4af0090f26434c24
Palak Garg <palakgarg296@gmail.com>
2022-07-07 12:20:37 +0530

    Time: 28 ms (5.25%), Space: 9.1 MB (29.13%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,16 +1,46 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int a=nums1.size()-m;
-      while(a--)
-      {
-        nums1.pop_back();
-      }
-      for(int i=0;i<nums2.size();i++)
-      {
-        nums1.push_back(nums2[i]);
-      }
-    
-       sort(nums1.begin(),nums1.end());
+        int c=m+n;
+        cout<<c;
+        for(int i=0;i<nums2.size();i++)
+        {
+            nums1[m+i]=nums2[i];
+        }
+        int gap;
+        if(c%2==0) gap=c/2;
+        else gap=c/2+1;
+        cout<<gap<<" ";
+        while(gap!=1)
+        {
+            
+            int i=0;
+            int j=gap;
+            while(j<=nums1.size()-1)
+            {
+                if(nums1[i]>nums1[j])
+             swap(nums1[i],nums1[j]);
+                i++; j++;
+            }
+             if(gap%2==0) gap=gap/2;
+        else gap=gap/2+1;
+            //cout<<gap<<" ";
+            for(int i=0;i<nums1.size();i++)
+            {
+                cout
+                    <<nums1[i]<<" ";
+               
+            }
+             cout<<endl;
+        }
+        int i=0;
+            int j=gap;
+            while(j<=nums1.size()-1)
+            {
+                if(nums1[i]>nums1[j])
+             swap(nums1[i],nums1[j]);
+                i++; j++;
+            }
+//return nums1;
     }
 };
