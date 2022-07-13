9ad402138478c8c5c1f555bff224ec003a549f39
Palak Garg <palakgarg296@gmail.com>
2022-07-13 16:29:54 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (29.13%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,32 +1,16 @@
 class Solution {
 public:
-   
-        
  void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int k=0;
-        for(int i=m;i<m+n;++i){
-          nums1[i]=nums2[k++];
-      }
-        
-        int gap=ceil((m+n)/2);
-        while(gap>0){
-           
-            int i=0,j=gap;
-            while(j<m+n && i<m+n){
-                if(nums1[i]>nums1[j]){
-                    swap(nums1[i],nums1[j]);     
-                }
-               i++;
-               j++;
-                
-            }
-            if (gap <= 1)
-              gap= 0;
-            else
-            gap=(gap / 2) + (gap % 2); // always gives the ceil value
-            
-        }
- 
-        
-    }
+     int i = m - 1, j = n - 1, k = m + n - 1;
+     while(i >= 0 and j >= 0) {
+         if(nums1[i] < nums2[j]) {
+             nums1[k--] = nums2[j--];
+         } else {
+             nums1[k--] = nums1[i--];
+         }
+     }
+     while(j >= 0) {
+         nums1[k--] = nums2[j--];
+     }
+ }
 };
