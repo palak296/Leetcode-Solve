12573542157be94901b5ea5e93935356768ca52b
Palak Garg <palakgarg296@gmail.com>
2022-07-21 17:01:55 +0530

    Time: 5 ms (23.08%), Space: 7.6 MB (75.68%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int s;
-        int mx=INT_MIN;
-         int n=nums.size();
-        vector<int>v(n+1,-1);
-        s=max(fun(nums,mx,0,v),fun(nums,mx,1,v));
-        return s;
+      int n=nums.size();
+        int prev=nums[0];
+        int prev2=0;
+        for(int i=1;i<n;i++)
+        {
+            int take=nums[i];
+            if(i>1) take+=prev2;
+            int notake=prev;
+            int cur=max(take,notake);
+            prev2=prev;
+            prev=cur;
+        }
+        return prev;
     }
-    int fun(vector<int>&nums, int mx, int i,vector<int>&v)
-    {
-        if(i>=nums.size()) return 0;
-        mx=nums[i];
-       if(v[i]!=-1) return v[i];
-            mx=mx+max(fun(nums,mx,i+2,v),fun(nums,mx,i+3,v));
-        
-        return v[i]=mx;
-    }
-    
+ 
 };
