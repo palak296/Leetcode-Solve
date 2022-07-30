c89c6883b3302c90a2b8d037bc99799ff030b93d
Palak Garg <palakgarg296@gmail.com>
2022-07-30 11:14:41 +0530

    Time: 0 ms (100.00%), Space: 6 MB (40.15%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,13 +1,12 @@
-vector<int>dp(100001,-1);
 class Solution {
 public:
-    
     int climbStairs(int n) {
-        
-        if(n<=2) return n;
-        if(dp[n]!=-1) return dp[n];
-        else
-       return dp[n]=climbStairs(n-1)+climbStairs(n-2);
-       
+        int dp[n+1];
+        dp[0]=dp[1]=1;
+       for(int i=2; i<=n; i++){
+      dp[i] = dp[i-1]+ dp[i-2];
+  }
+  cout<<dp[n];
+  return dp[n];
     }
 };
