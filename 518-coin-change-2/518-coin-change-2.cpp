bc914634518a46d3848aa634b1de3cad3635cb7c
Palak Garg <palakgarg296@gmail.com>
2022-08-04 15:54:56 +0530

    Time: 64 ms (30.00%), Space: 18.9 MB (19.01%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -4,23 +4,25 @@ public:
         if(a==0 ) return 1;
         int n=c.size();
         vector<vector<int>>dp(n,vector<int>(a+1,0));
+        vector<int>prev(a+1); vector<int>cur(a+1);
         for(int i=0; i<=a; i++) 
         {
-            dp[0][i] = (i%c[0]==0); 
+            prev[i] = (i%c[0]==0); 
         }
         for(int i=1;i<n;i++)
         {
             
             for(int j=0; j<=a; j++)
             {
-                int dontpick = dp[i-1][j];               int pick = 0;
+                int dontpick = prev[j];               int pick = 0;
                 if(c[i]<=j)
                 {
-                    pick = dp[i][j-c[i]];
+                    pick = cur[j-c[i]];
                 }
-                dp[i][j] = dontpick + pick;
+                cur[j] = dontpick + pick;
             }
+            prev=cur;
         }
-        return dp[n-1][a];
+        return prev[a];
     }
 };
