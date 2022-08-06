fce14314b6ac9c62ac5f4124775de39862c2da98
Palak Garg <palakgarg296@gmail.com>
2022-08-06 12:34:09 +0530

    Time: 5 ms (33.65%), Space: 6.2 MB (48.48%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+   int numDecodings(string s) {
+        if(s[0] == '0')return 0;
+        if(s.size() == 1)return 1;
+        int last = 1, secondlast = 1;
+        for(int i = 1; i<s.size(); i++){
+            if(s[i] == '0')last = 0;
+            if(s[i-1] == '1' || s[i-1] == '2' && s[i] <= '6'){
+                last = last + secondlast;
+                secondlast = last - secondlast;
+            }
+            else
+                secondlast = last;
+        }
+        return last;
+    }
+};
