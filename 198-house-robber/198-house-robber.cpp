db91281fc5384ffd827c0ce304e0a3a076dd40b9
Palak Garg <palakgarg296@gmail.com>
2022-08-06 09:37:40 +0530

    Time: 2 ms (58.50%), Space: 7.7 MB (76.61%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -3,18 +3,17 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
           if(size(nums) == 1) return nums[0];
-        vector<int>dp(n);
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
-        
+        int prev2=nums[0],  prev=max(nums[0],nums[1]),cur;
         int take, notake;
         for(int i=2;i<n;i++)
         {
-             take=nums[i]+dp[i-2];
+             take=nums[i]+prev2;
         
-         notake=dp[i-1];
-            dp[i]=max(take,notake);
+         notake=prev;
+            cur=max(take,notake);
+            prev2=prev;
+            prev=cur;
         }
-        return dp[n-1];
+        return prev;
     }
 };
