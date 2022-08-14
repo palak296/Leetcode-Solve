5947e37f3765ae03e2572146c376cdcd209c9415
Palak Garg <palakgarg296@gmail.com>
2022-08-14 12:37:22 +0530

    Time: 31 ms (77.04%), Space: 13.7 MB (30.40%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        int s=graph.size();
-        vector<int>vis(s,-1);
-        queue<int>q;
-        for(int i=0;i<s;i++)
-        {
-            if(vis[i]==-1){
-                
-        q.push(i);
-        vis[i]=0;
-                 while(!q.empty())
-        {
-            int n=q.front();
-            q.pop();
-            for(auto it: graph[n])
-            {
-                if(vis[it]==-1)
-                {
-                    vis[it]=1-vis[n];
-                    q.push(it);
-                }
-                else 
-                    if(vis[it]==vis[n]) return false;
-            }
-        }
-            }
-        }
-       
+        int n=graph.size();
+        vector<int>vis(n,-1);
+       for(int i=0;i<n;i++)
+       {
+           if(vis[i]==-1)
+           {
+               if(!bfs(i,graph,vis)) return false;
+           }
+       }
         return true;
     }
-   
+    bool bfs(int node, vector<vector<int>>&graph, vector<int>&vis)
+    {
+        vis[node]=1;
+        queue<int>q;
+        q.push(node);
+            while(!q.empty())
+            {
+                int n=q.front();
+                
+                q.pop();
+                for(auto it: graph[n])
+                {
+                    if(vis[it]==-1)
+                    {
+                        vis[it]=1-vis[n];
+                        q.push(it);
+                    }
+                    else if(vis[it]==vis[n]) return false;
+                    
+                }
+            }
+        return true;
+    }
 };
