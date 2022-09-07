96691fdb40b3cb62df080db86ea43229437fe1ac
Palak Garg <palakgarg296@gmail.com>
2022-09-08 04:40:30 +0530

    Time: 6 ms (87.85%), Space: 13 MB (79.62%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,67 +1,50 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
+     int orangesRotting(vector<vector<int>>& grid) 
+    {
+        
+        vector<int> dir={-1,0,1,0,-1}; //used for finding all 4 adjacent coordinates
         
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<int>>vis=grid;
-        int cnt=0;
-        queue<pair<int,int>>q;
+        
+        queue<pair<int,int>> q;
+        int fresh=0; //To keep track of all fresh oranges left
         for(int i=0;i<m;i++)
-        {
             for(int j=0;j<n;j++)
             {
-                if(grid[i][j]==2 )
-                {   
-                 
-                   q.push({i,j});
-                }
+                if(grid[i][j]==2)
+                    q.push({i,j});
+                if(grid[i][j]==1)
+                    fresh++;
             }
-        }
-        
+        int ans=-1; //initialised to -1 since after each step we increment the time by 1 and initially all rotten oranges started at 0.
         while(!q.empty())
         {
-           
-            int s=q.size();
-            int flag=1;
-            for(int i=0;i<s;i++)
+            int sz=q.size();
+            while(sz--)
             {
-                 int r=q.front().first;
-                 int c=q.front().second;
-                 q.pop();
-                
-                if(r+1<m and grid[r+1][c]==1 and vis[r+1][c]!=2)
+                pair<int,int> p=q.front();
+                q.pop();
+                for(int i=0;i<4;i++)
                 {
-                    vis[r+1][c]=2; q.push({r+1,c}); flag=0;
+                    int r=p.first+dir[i];
+                    int c=p.second+dir[i+1];
+                    if(r>=0 && r<m && c>=0 && c<n &&grid[r][c]==1)
+                    {
+                        grid[r][c]=2;
+                        q.push({r,c});
+                        fresh--; // decrement by 1 foreach fresh orange that now is rotten
+                    }
+                    
                 }
-        if(c+1<n and grid[r][c+1]==1 and vis[r][c+1]!=2)
-        {
-            vis[r][c+1]=2; q.push({r,c+1}); flag=0;
-        }
-        if(r-1>=0 and grid[r-1][c]==1 and vis[r-1][c]!=2)
-        {
-            vis[r-1][c]=2; q.push({r-1,c}); flag=0;
-        }
-        if(c-1>=0 and grid[r][c-1]==1 and vis[r][c-1]!=2)
-        {
-            vis[r][c-1]=2; q.push({r,c-1}); flag=0;
-        }
-                 
-            }if(flag==0) {cout<<flag; cnt++;}
-           
-            
-        }
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-               // cout<<vis[i][j]<<" ";
-                if(vis[i][j]==1) return -1;
             }
-            cout<<endl;
+            ans++; //incremented after each minute passes
         }
-        return cnt;
+        if(fresh>0) return -1; //if fresh>0 that means there are fresh oranges left
+        if(ans==-1) return 0; //we initialised with -1, so if there were no oranges it'd take 0 mins.
+        return ans;
+        
     }
-
     
 };
