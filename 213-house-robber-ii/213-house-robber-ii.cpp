95e0c639f40bed1ee16916207a5d53055af4cc49
Palak Garg <palakgarg296@gmail.com>
2022-09-08 10:00:05 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (86.24%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    int rec(vector<int>&nums,int n,vector<int>&dp){
-        if(n<0) return 0;
-        int t,nt;
-         if(dp[n]!=-1)
-        {
-            return dp[n];
-        }
-            t=rec(nums,n-2,dp)+nums[n];
-            nt=rec(nums,n-1,dp);
-        
-        return dp[n]=max(t,nt);
-    }
     int rob(vector<int>& nums) {
-        vector<int>n1;
-        vector<int>n2;
-        int n=nums.size();
+        if(nums.empty()) return 0;
+        if(nums.size() == 1) return nums[0];
+        vector<int>v1,v2;
         for(int i=0;i<nums.size();i++)
         {
-            if(i==0) n1.push_back(nums[i]);
-            else if(i==n-1) n2.push_back(nums[i]);
-            else { n1.push_back(nums[i]); n2.push_back(nums[i]);}
+            if(i!=0) v2.push_back(nums[i]);
+            if(i!=nums.size()-1) v1.push_back(nums[i]);
         }
-         vector<int> dp1(n1.size() , -1);
-        vector<int> dp2(n2.size() , -1);
-        return max(rec(n1,n1.size()-1,dp1),rec(n2,n2.size()-1,dp2));
+        return max(rec(v1),rec(v2));
+    }
+
+
+    int rec(vector<int>& nums) {
+        int n=nums.size();
+          if(size(nums) == 1) return nums[0];
+        int prev2=nums[0],  prev=max(nums[0],nums[1]),cur;
+        int take, notake;
+        for(int i=2;i<n;i++)
+        {
+             take=nums[i]+prev2;
+        
+         notake=prev;
+            cur=max(take,notake);
+            prev2=prev;
+            prev=cur;
+        }
+        return prev;
     }
 };
